fd3267910ff17db9b0af0008edeaae698f3cb7de
rajashekar8899 <8899rajashekar@gmail.com>
2026-01-29 01:23:21 +0530

    Automation: Restoring full autonomous engine with clean syntax

--- cleanup_runs.ps1
+++
@@ -1,7 +0,0 @@
-$ids = gh run list --status failure --limit 50 --json databaseId -q '.[] | .databaseId'
-foreach ($id in $ids) {
-    if ($id) {
-        Write-Host "Deleting run $id"
-        gh run delete $id
-    }
-}
